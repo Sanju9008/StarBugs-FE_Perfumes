319dc2a53b3a737da714fd22b9dadaf3c5dc0543
Sanju9008 <kotisanjeev42@gmail.com>
2026-08-12 00:28:40 +0530

    Fix localhost API URL in production

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import './index.css'
 import axios from 'axios'
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL || '';
+// axios.defaults.baseURL = import.meta.env.VITE_API_URL || '';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
